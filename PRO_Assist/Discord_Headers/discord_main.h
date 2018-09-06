1f64c68bee2ec4f708c280a144e6e6a9cbb0b96a
Rafaella Mattos <39540116+RNozomi@users.noreply.github.com>
2018-09-06 08:27:45 -0300

    Initial Release

---
+++ PRO_Assist/Discord_Headers/discord_main.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <windows.h>
+#include <atlbase.h>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <thread>
+#include "discord_rpc.h"
+static const char* APPLICATION_ID = "486907050069327872";
+static int64_t StartTime;
+static int SendPresence = 1;
